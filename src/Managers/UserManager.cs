cec9df161396c8a78d1c74c44586682ab326feb1
Krolo <mateusz.krol.it@gmail.com>
2022-04-11 13:29:20 +0200

    Rework email validation

--- src/Managers/UserManager.cs
+++ src/Managers/UserManager.cs
@@ -1,12 +1,11 @@
-﻿using System.Text.RegularExpressions;
-
-using Stonks.Data;
+﻿using Stonks.Data;
 using Stonks.Helpers;
 
 namespace Stonks.Managers;
 public class UserManager : IUserManager
 {
 	private readonly AppDbContext _ctx;
+	private const int EMAIL_MAX_LENGTH = 64;
 
 	public UserManager(AppDbContext ctx)
 	{
@@ -28,13 +27,72 @@ public class UserManager : IUserManager
 
 	private static void ValidateEmail(string email)
 	{
-		if (email.Length > 64)
-			throw new ArgumentException("Email address is too long");
+		var length = email.Length;
 
-		var match = Regex.Match(email, @"^[a-z0-9.\-_]+@[a-z0-9\-]+\.[a-z0-9.\-]+$",
-			RegexOptions.Singleline, TimeSpan.FromMilliseconds(10));
+		if (length > EMAIL_MAX_LENGTH)
+			throw new EmailTooLongException(EMAIL_MAX_LENGTH);
 
-		if (!match.Success)
-			throw new ArgumentException($"Address: \"{email}\" is not valid");
+		var atIndex = CheckAlias(length, email);
+		CheckDomain(length, atIndex, email);
+	}
+
+	private static void ThrowError(string email)
+	{
+		throw new InvalidEmailException(email);
+	}
+
+	private static int CheckAlias(int length, string email)
+	{
+		if (!char.IsLetterOrDigit(email[0]))
+			ThrowError(email);
+
+		var currentChar = 1;
+		while (currentChar < length && email[currentChar] != '@')
+		{
+			if (!email[currentChar].IsAlphaNum() &&
+				email[currentChar] != '.' &&
+				email[currentChar] != '-' &&
+				email[currentChar] != '_')
+				ThrowError(email);
+
+			currentChar++;
+		}
+
+		if (currentChar + 1 >= length)
+			ThrowError(email);
+
+		return currentChar;
+	}
+
+	private static void CheckDomain(int length, int atIndex, string email)
+	{
+		var currentChar = length - 1;
+		var charsBetweenDots = 1;
+		atIndex++;
+
+		if (!char.IsLetter(email[currentChar]))
+			ThrowError(email);
+
+		while (currentChar != atIndex)
+		{
+			currentChar--;
+
+			if (char.IsLetter(email[currentChar]))
+			{
+				charsBetweenDots++;
+				continue;
+			}
+
+			if (email[currentChar] != '.')
+			{
+				ThrowError(email);
+			}
+
+			if (charsBetweenDots < 2) ThrowError(email);
+			charsBetweenDots = 0;
+		}
+
+		if (charsBetweenDots == length - atIndex)
+			ThrowError(email);
 	}
 }
